84104c03b8e94399fdef2df52a072c14eafb7429
Leebeth Lorena Anacona Chicangana <lorena_2328@hotmail.com>
2017-07-02 11:46:20 -0500

    Conexión exitosa de autenticación

--- HackHome/MainActivity.cs
+++ HackHome/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using HackAtHome.SAL;
+using HackAtHome.Entities;
 
 namespace HackHome
 {
@@ -11,7 +13,20 @@ namespace HackHome
         {
             base.OnCreate(bundle);
             SetContentView (Resource.Layout.Main);
+            var ButtonValidate = FindViewById<Button>(Resource.Id.buttonValidate);
+            ButtonValidate.Click += (sender, e) =>
+            {
+                var studentEmail = FindViewById<EditText>(Resource.Id.editTextEmail).Text;
+                var Passwd = FindViewById<EditText>(Resource.Id.editTextPassword).Text;
+                string Token = Validate(studentEmail, Passwd);
 
+            };
+        }
+        private async string Validate(string studentEmail, string password)
+        {
+            var ServiceCliente = new ServiceClient();            
+            ResultInfo Result = await ServiceCliente.AutenticateAsync(studentEmail,password);
+            return Result.Token;
         }
     }
 }
