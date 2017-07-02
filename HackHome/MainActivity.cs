69e1dfc5d565db53584ce601ba30c945e3833047
Leebeth Lorena Anacona Chicangana <lorena_2328@hotmail.com>
2017-07-02 11:09:35 -0500

    Import Project

---
+++ HackHome/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HackHome
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/MyIcon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+
+        }
+    }
+}
+
