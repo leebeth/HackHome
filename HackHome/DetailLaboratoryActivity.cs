ba36aeedcb4776fefd52ab174316e1cb17a6bc2f
Leebeth Lorena Anacona Chicangana <lorena_2328@hotmail.com>
2017-07-02 19:56:14 -0500

    Correciones para alineamiento

--- HackHome/DetailLaboratoryActivity.cs
+++ HackHome/DetailLaboratoryActivity.cs
@@ -15,54 +15,42 @@ using HackAtHome.Entities;
 
 namespace HackHome
 {
-    [Activity(Label = "DetailLaboratoryActivity")]
+    [Activity(Label = "@string/ApplicationName", Icon = "@drawable/MyIcon")]
     public class DetailLaboratoryActivity : Activity
     {
-        public const string ID_DETAIL_LABORATORY = "IdDetail";
-        public const string TOKEN = "token";
-        public const string TITTLE = "tittle";
-        public const string STATUS = "status";
-        public const string USERNAME = "username";
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             SetContentView(Resource.Layout.DetailLaboratory);
 
-            InitComponents(Intent.GetIntExtra(ID_DETAIL_LABORATORY, 0), Intent.GetStringExtra(TOKEN),
-                Intent.GetStringExtra(TITTLE), Intent.GetStringExtra(STATUS), Intent.GetStringExtra(USERNAME));
+            var IdEvidenceDetail = Intent.Extras.GetInt("IdDetail");
+            var Token = Intent.Extras.GetString("token");
+            var Tittle = Intent.Extras.GetString("tittle");
+            var Status = Intent.Extras.GetString("status");
+            var Username = Intent.Extras.GetString("username");
+
+            GetEvidenceDetail(IdEvidenceDetail,Token,Title,Status,Username);
         }
 
-        private async void InitComponents(int idEvidenceDetail, string token, string tittle, string status, string username)
+        private async void GetEvidenceDetail(int idEvidenceDetail, string token, string tittle, string status, string username)
         {
             ServiceClient service = new ServiceClient();
             EvidenceDetail detail = await service.GetEvidenceByIDAsync(token, idEvidenceDetail);
 
-            #region Username
             var usernameView = FindViewById<TextView>(Resource.Id.UserName);
             usernameView.Text = username;
-            #endregion
 
-            #region Tittle
             var tittleView = FindViewById<TextView>(Resource.Id.ActivityName);
             tittleView.Text = tittle;
-            #endregion
 
-            #region Status
             var statusView = FindViewById<TextView>(Resource.Id.ActivityStatus);
             statusView.Text = status;
-            #endregion
-
-            #region Description
+            
             var DescriptionWebView = FindViewById<WebView>(Resource.Id.Descripcion);
             DescriptionWebView.LoadDataWithBaseURL(null, detail.Description, "text/html", "uft-8", null);
-            #endregion
-
-            #region Image            
+           
             var ImageEvidence = FindViewById<ImageView>(Resource.Id.ImageEvidence);
-            Koush.UrlImageViewHelper.SetUrlDrawable(ImageEvidence, detail.Url);
-            #endregion
+            Koush.UrlImageViewHelper.SetUrlDrawable(ImageEvidence, detail.Url);            
         }
     }
 }
--- HackHome/EvidenceListActivity.cs
+++ HackHome/EvidenceListActivity.cs
@@ -17,6 +17,14 @@ namespace HackHome
     [Activity(Label = "@string/ApplicationName", Icon = "@drawable/MyIcon")]
     public class EvidenceListActivity : Activity
     {
+        public const string ID_DETAIL_LABORATORY = "IdDetail";
+        public const string TOKEN = "token";
+        public const string TITTLE = "tittle";
+        public const string STATUS = "status";
+        public const string USERNAME = "username";
+
+        Complex Data;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,16 +33,57 @@ namespace HackHome
             var FullName = Intent.Extras.GetString("fullName");
             var TextViewEmail = FindViewById<TextView>(Resource.Id.textViewFullName);
             TextViewEmail.Text = FullName;
-            GetEvidences(Token);
+
+            Data = (Complex)this.FragmentManager.FindFragmentByTag("FragmentData");
+            if (Data == null)
+            {
+                Data = new Complex();
+                var FragmentTransaction = this.FragmentManager.BeginTransaction();
+                FragmentTransaction.Add(Data, "FragmentData");
+                FragmentTransaction.Commit();
+            }
+            if (Data.Evidences == null || Data.Evidences.Count == 0)
+            {
+                Android.Util.Log.Debug("lab11log", "Activity A - GetEvidences");
+                GetEvidences(Token, FullName);
+            }
+            else
+            {
+                Android.Util.Log.Debug("lab11log", "Activity A - buildEvidence");
+                buildEvidence(Data.Evidences, Token, FullName);
+            }
         }
 
-        private async void GetEvidences(string token)
+        private async void GetEvidences(string token, string fullName)
         {
             var ServiceCliente = new HackAtHome.SAL.ServiceClient();
             List<Evidence> Result = await ServiceCliente.GetEvidencesAsync(token);
+            Data.Evidences = Result;
+            buildEvidence(Result,token,fullName);            
+        }
 
+        private void buildEvidence(List<Evidence> result, string token, string fullName)
+        {
             var ListViewEvidences = FindViewById<ListView>(Resource.Id.listViewEvidences);
-            ListViewEvidences.Adapter = new EvidencesAdapter(this, Result, Resource.Layout.EvidenceItem, Resource.Id.textViewName, Resource.Id.textViewStatus);
+            ListViewEvidences.Adapter = new EvidencesAdapter(this, result, Resource.Layout.EvidenceItem, Resource.Id.textViewName, Resource.Id.textViewStatus);
+            ListViewEvidences.ItemClick += (sender, e) =>
+            {
+                var Index = e.Position;
+                Evidence evidence = result[Index];
+                Intent Intent = new Intent(this, typeof(DetailLaboratoryActivity));
+                Intent.PutExtra(ID_DETAIL_LABORATORY, evidence.EvidenceID);
+                Intent.PutExtra(TITTLE, evidence.Title);
+                Intent.PutExtra(STATUS, evidence.Status);
+                Intent.PutExtra(TOKEN, token);
+                Intent.PutExtra(USERNAME, fullName);
+                StartActivity(Intent);
+            };
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            Android.Util.Log.Debug("lab11log", "Activity A - OnSaveInstanceState");
+            base.OnSaveInstanceState(outState);
         }
     }
 }
